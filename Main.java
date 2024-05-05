3b16358cfc306c76da9db412999c9abaa93c282b
Oliwia Kowalczyk <oliwia.szelag@op.pl>
2024-05-05 14:51:47 +0200

    chore: add new file

--- Main.java
+++ Main.java
@@ -1,14 +1,11 @@
-import company.beans.Person;
-import company.exceptions.InvalidAgeException;
+
+import company.implementations.EmailMessenger;
+import company.interfaces.Messenger;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            Person person = new Person("Anna Te", 24);
-            System.out.println("Name;" + person.getName());
-            System.out.println("Age;" + person.getAge());
-        } catch (InvalidAgeException e) {
-            System.out.println("Invalid age provided:" + e.getMessage());
-        }
+        Messenger messenger = new EmailMessenger();
+        messenger.sendMessage("Test");
+
     }
 }
