0dd12002ca647767f0a0fb8e5b915aa05909ea7d
Oliwia Kowalczyk <oliwia.szelag@op.pl>
2024-05-05 15:05:03 +0200

    chore: add new file

--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
 
 import company.implementations.EmailMessenger;
 import company.interfaces.Messenger;
+import company.utils.mathUtils;
 
 public class Main {
     public static void main(String[] args) {
+        int result = mathUtils.add(3, 4);
+        System.out.println("Wynik:" + result);
         Messenger messenger = new EmailMessenger();
-        messenger.sendMessage("Test");
+        messenger.sendMessage("test");
 
     }
 }
---
+++ company/utils/mathUtils.java
@@ -0,0 +1,9 @@
+package company.utils;
+
+public class mathUtils {
+    public static int add(int a, int b) {
+        return a + b;
+
+    }
+
+}
